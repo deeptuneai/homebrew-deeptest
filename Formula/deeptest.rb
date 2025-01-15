a3f7a2ed67f36978cbf26c5ef5f7cb6569f25308
Ashley Wong <ashleyltwong@gmail.com>
2025-01-15 15:50:59 -0500

    bump python to 3.9

--- Formula/deeptest.rb
+++ Formula/deeptest.rb
@@ -7,7 +7,7 @@ class Deeptest < Formula
     sha256 "397323a4ee2f6c90734f2b2a61b9fdf602dcea85ee9d11128a6cbbc2038353a0"
     license "Proprietary"
   
-    depends_on "python@3.8"
+    depends_on "python@3.9"
   
     resource "click" do
       url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
