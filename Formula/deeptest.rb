126b0ae5cc60d9211361e0dca2d9de94f7baed98
Ashley Wong <ashleyltwong@gmail.com>
2025-01-15 15:54:26 -0500

    bump sha

--- Formula/deeptest.rb
+++ Formula/deeptest.rb
@@ -21,12 +21,12 @@ class Deeptest < Formula
   
     resource "keyring" do
       url "https://files.pythonhosted.org/packages/70/09/d904a6e96f76ff214be59e7aa6ef7190008f52a0ab6689760a98de0bf37d/keyring-25.6.0.tar.gz"
-      sha256 "ca0746a19ec421219f4d713f848fa297a661a8a8c1504867e55bfb5e09091509"
+      sha256 "0b39998aa941431eb3d9b0d4b2460bc773b9df6fed7621c2dfb291a7e0187a66"
     end
   
     resource "typer" do
       url "https://files.pythonhosted.org/packages/cb/ce/dca7b219718afd37a0068f4f2530a727c2b74a8b6e8e0c0080a4c0de4fcd/typer-0.15.1.tar.gz"
-      sha256 "50922fd79aea2f4751a8e0408ff10d2662bd0c8bbfa84755a699f3bada2978b2"
+      sha256 "a0588c0a7fa68a1978a069818657778f86abe6ff5ea6abf472f940a08bfe4f0a"
     end
   
     def install
